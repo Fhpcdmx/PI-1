7e732bd7879cccb7dceb280f26309fb148dc8d00
Fernando Hernandez <152935483+Fhpcdmx@users.noreply.github.com>
2025-04-01 22:33:29 -0600

    Update recomendacion.py

--- src/recomendacion.py
+++ src/recomendacion.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
 
-peliculas_df = pd.read_csv('../data/movies_dataset.csv', encoding='latin1')
+peliculas_df = pd.read_csv('data/movies_dataset.csv', encoding='latin1')
 
 
 tfidf = TfidfVectorizer(stop_words='english')
@@ -21,10 +21,23 @@ app = FastAPI()
 
 @app.get("/")
 def home():
+    """
+    Ruta raíz de la API.
+    Retorna un mensaje de bienvenida en formato JSON.
+    """
     return {"message": "API de Recomendación de Películas"}
 
 @app.get('/cantidad_filmaciones_mes/{mes}')
 def cantidad_filmaciones_mes(mes: str):
+    """
+    Obtiene la cantidad de películas estrenadas en un mes específico.
+    
+    Parámetros:
+        mes (str): Nombre del mes en español (Ej: 'Enero', 'Febrero').
+    
+    Retorno:
+        dict: Mensaje con la cantidad de películas estrenadas en el mes indicado.
+    """
     meses_espanol = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 
                      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
     
@@ -39,6 +52,15 @@ def cantidad_filmaciones_mes(mes: str):
 
 @app.get('/cantidad_filmaciones_dia/{dia}')
 def cantidad_filmaciones_dia(dia: str):
+    """
+    Obtiene la cantidad de películas estrenadas en un día de la semana específico.
+
+    Parámetros:
+        dia (str): Nombre del día en español (Ej: 'lunes', 'martes').
+
+    Retorno:
+        dict: Mensaje con la cantidad de películas estrenadas en el día indicado.
+    """
     dias_esp = {
         'Monday': 'lunes', 'Tuesday': 'martes', 'Wednesday': 'miércoles',
         'Thursday': 'jueves', 'Friday': 'viernes', 'Saturday': 'sabado', 'Sunday': 'domingo'
@@ -55,6 +77,15 @@ def cantidad_filmaciones_dia(dia: str):
 
 @app.get('/score_titulo/{titulo}')
 def score_titulo(titulo: str):
+    """
+    Obtiene el score y los datos generales de una pelicula a partir de el titulo.
+
+    Parámetros:
+        dia (str): titulo de la pelicula.
+
+    Retorno:
+        dict: titulo de la pelicula, año de esteno y score.
+    """
     pelicula = peliculas_df[peliculas_df['title'].str.contains(titulo, case=False, na=False)]
     
     if pelicula.empty:
@@ -69,6 +100,15 @@ def score_titulo(titulo: str):
 
 @app.get('/votos_titulo/{titulo}')
 def votos_titulo(titulo: str):
+    """
+    Obtiene la cantidad de votos y el promedio de votos de una película si tiene al menos 2000 valoraciones.
+
+    Parámetros:
+        titulo (str): Título de la película.
+
+    Retorno:
+        dict: Título de la película, cantidad de votos y promedio de votos.
+    """
     pelicula = peliculas_df[peliculas_df['original_title'].str.contains(titulo, case=False, na=False)]
     
     if pelicula.empty:
@@ -87,7 +127,16 @@ def votos_titulo(titulo: str):
 
 @app.get('/get_actor/{nombre_actor}')
 def get_actor(nombre_actor: str):
-    actores_df = pd.read_csv('../data/cast.csv')
+    """
+    Obtiene la cantidad de películas en las que ha participado un actor y el promedio de retorno.
+
+    Parámetros:
+        nombre_actor (str): Nombre del actor.
+
+    Retorno:
+        dict: Nombre del actor, cantidad de películas y promedio de retorno.
+    """
+    actores_df = pd.read_csv('data/cast.csv')
     actor_peliculas = actores_df[actores_df['actor_name'].str.contains(nombre_actor, case=False, na=False)]
     
     if actor_peliculas.empty:
@@ -105,7 +154,17 @@ def get_actor(nombre_actor: str):
 
 @app.get('/get_director/{nombre_director}')
 def get_director(nombre_director: str):
-    directores_df = pd.read_csv('../data/crew.csv')
+    """
+    Obtiene las películas dirigidas por un director, incluyendo título, fecha de lanzamiento, ingresos,
+    compañías de producción, países de producción y promedio de votos.
+
+    Parámetros:
+        nombre_director (str): Nombre del director.
+
+    Retorno:
+        dict: Nombre del director y una lista de películas con su información.
+    """
+    directores_df = pd.read_csv('data/crew.csv')
     director_peliculas = directores_df[
         (directores_df['crew_name'].str.contains(nombre_director, case=False, na=False)) &
         (directores_df['job'] == 'Director')
@@ -124,6 +183,15 @@ def get_director(nombre_director: str):
 
 @app.get('/recomendacion/{titulo}')
 def recomendacion(titulo: str):
+    """
+    Genera recomendaciones de películas similares a la película indicada basado en el genero titulo y popularidad.
+
+    Parámetros:
+        titulo (str): Título de la película para generar recomendaciones.
+
+    Retorno:
+        dict: Lista de títulos de películas recomendadas.
+    """
     if titulo not in indices:
         raise HTTPException(status_code=404, detail="La película no se encuentra en la base de datos.")
 
